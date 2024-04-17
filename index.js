38528fb4d49a16a53245904c03b2194dc5da32c5
lac1990 <155014030+lac1990@users.noreply.github.com>
2024-04-17 13:23:17 -0500

    2.80

--- index.js
+++ index.js
@@ -2,36 +2,18 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 uuid = require("uuid");
-<<<<<<< HEAD
 const mongoose = require("mongoose");
 const Models = require("./models");
 const Movies = Models.Movie;
 const Users = Models.User;
 
 mongoose.connect("mongodb://localhost:27017/cf_movies", {
-=======
-const morgan = require ("morgan");
-const mongoose = require("mongoose");
-const Models = require("./models");
-
-
-const Movies = Models.Movie;
-const Users = Models.User;
-const Genres = Models.Genre;
-const Directors = Models.Director;
-
-mongoose.connect("mongodb://localhost:27017/moviedb", {
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 // Middleware 
 
-<<<<<<< HEAD
-// Middleware 
-
-=======
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -39,34 +21,21 @@ app.use(bodyParser.json());
 
 //CREATE: Add a user
 
-<<<<<<< HEAD
 app.post("/users", async (req, res) => {
   await Users.findOne({
-=======
-app.post("/user", async (req, res) => {
-  await User.findOne({
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
       Username: req.body.Username
     })
     .then((user) => {
       if (user) {
         return res.status(400).send(req.body.Username + "already exists");
       } else {
-<<<<<<< HEAD
         Users.create({
-=======
-        User.create({
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
             Username: req.body.Username,
             Password: req.body.Password,
             Email: req.body.Email,
             Birthday: req.body.Birthday,
           })
-<<<<<<< HEAD
           .then((user) => {
-=======
-          .then((Users) => {
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
             res.status(201).json(user);
           })
           .catch((error) => {
@@ -83,13 +52,8 @@ app.post("/user", async (req, res) => {
 
 // Update: a user's info, by username
 
-<<<<<<< HEAD
 app.put("/users/:Username", async (req, res) => {
   await Users.findOneAndUpdate({
-=======
-app.put("/user/:Username", async (req, res) => {
-  await User.findOneAndUpdate({
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
       Username: req.params.Username
     }, {
       $set: {
@@ -112,13 +76,8 @@ app.put("/user/:Username", async (req, res) => {
 
 // Add a movie to a user's list of favorites
 
-<<<<<<< HEAD
 app.post("/users/:Username/movies/:MovieID", async (req, res) => {
   await Users.findOneAndUpdate({
-=======
-app.post("/user/:Username/movie/:MovieID", async (req, res) => {
-  await User.findOneAndUpdate({
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
       Username: req.params.Username
     }, {
       $push: {
@@ -138,27 +97,16 @@ app.post("/user/:Username/movie/:MovieID", async (req, res) => {
 
 //delete a movie from favorites
 
-<<<<<<< HEAD
 app.delete("/users/:id/:movieTitle", (req, res) => {
-=======
-app.delete("/user/:id/:movieTitle", (req, res) => {
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
   const {
     id,
     movieTitle
   } = req.params;
 
-<<<<<<< HEAD
   let user = users.find((user) => user.id == id);
 
   if (user) {
     user.favMovies = user.favMovies.filter((title) => title !== movieTitle);
-=======
-  let user = user.find((usesr) => user.id == id);
-
-  if (user) {
-    user.favMovies = users.favMovies.filter((title) => title !== movieTitle);
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
     res
       .status(200)
       .send(`${movieTitle} has been removed from user ${id}'s array`);
@@ -169,11 +117,7 @@ app.delete("/user/:id/:movieTitle", (req, res) => {
 
 //delete user
 
-<<<<<<< HEAD
 app.delete("/users/:Username", (req, res) => {
-=======
-app.delete("/user/:Username", (req, res) => {
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
   Users.findOneAndRemove({
       Username: req.params.userName
     })
@@ -193,26 +137,15 @@ app.delete("/user/:Username", (req, res) => {
 //READ
 
 app.get("/", (req, res) => {
-<<<<<<< HEAD
   res.send("Top Tem");
-=======
-  res.send("Welcome to MyFlix");
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
 });
 
 // Get all users
 
-<<<<<<< HEAD
 app.get("/users", async (req, res) => {
   await Users.find()
     .then((users) => {
       res.status(201).json(users);
-=======
-app.get("/user", async (req, res) => {
-  await User.find()
-    .then((user) => {
-      res.status(201).json(user);
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
     })
     .catch((err) => {
       console.error(err);
@@ -222,13 +155,8 @@ app.get("/user", async (req, res) => {
 
 // Get a user by username
 
-<<<<<<< HEAD
 app.get("/users/:Username", async (req, res) => {
   await Users.findOne({
-=======
-app.get("/user/:Username", async (req, res) => {
-  await User.findOne({
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
       Username: req.params.Username
     })
     .then((user) => {
@@ -242,11 +170,7 @@ app.get("/user/:Username", async (req, res) => {
 
 //get movies
 
-<<<<<<< HEAD
 app.get("/movies", async (req, res) => {
-=======
-app.get("/movie", async (req, res) => {
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
   await Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
@@ -259,11 +183,7 @@ app.get("/movie", async (req, res) => {
 
 // get by title
 
-<<<<<<< HEAD
 app.get("/movies/:Title", async (req, res) => {
-=======
-app.get("/movie/:Title", async (req, res) => {
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
   await Movies.findOne({
       Title: req.params.Title
     })
@@ -278,11 +198,7 @@ app.get("/movie/:Title", async (req, res) => {
 
 //get genre
 
-<<<<<<< HEAD
 app.get("/movies/genres/:genreName", async (req, res) => {
-=======
-app.get("/movie/genre/:genreName", async (req, res) => {
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
   await Movies.findOne({
       "Genre.Name": req.params.genreName
     })
@@ -297,11 +213,7 @@ app.get("/movie/genre/:genreName", async (req, res) => {
 
 //get director
 
-<<<<<<< HEAD
 app.get("/movies/directors/:directorName", async (req, res) => {
-=======
-app.get("/movie/director/:directorName", async (req, res) => {
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
   await Movies.findOne({
       "Director.Name": req.params.directorName
     })
--- models.js
+++ models.js
@@ -26,11 +26,6 @@ let movieSchema = mongoose.Schema({
   let Movie = mongoose.model('Movie', movieSchema);
   let User = mongoose.model('User', userSchema);
   
-<<<<<<< HEAD
   module.exports.Movie = Movie;
   module.exports.User = User;
-=======
-  module.exports.Movie = this.Movie;
-  module.exports.User = this.User;
->>>>>>> 0442b738861bbd18ea78fe896dde3ea00a6b1b55
   
