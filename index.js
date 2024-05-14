2daefdad5ef6eb918d91dca3bccb3ebeaf041509
lac1990 <155014030+lac1990@users.noreply.github.com>
2024-05-14 13:43:06 -0500

    format update

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express'),
-morgan = require('morgan'),
-path = require('path'),
-bodyParser = require('body-parser'),
-mongoose = require('mongoose'),
-Models = require('./models.js');
+  morgan = require('morgan'),
+  path = require('path'),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
+  Models = require('./models.js');
 uuid = require('uuid')
 fs = require('fs');
 
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const app = express();
 
@@ -35,97 +38,111 @@ const {
 } = require('express-validator');
 
 // create a write stream
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
+  flags: 'a'
+})
 // setup the logger
-app.use(morgan('combined', {stream: accessLogStream}));
+app.use(morgan('combined', {
+  stream: accessLogStream
+}));
 
-app.post('/users', 
+app.post('/users',
   [
-    check('Username', 'Username is required').isLength({min: 5}),
+    check('Username', 'Username is required').isLength({
+      min: 5
+    }),
     check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
     check('Password', 'Password is required').not().isEmpty(),
     check('Email', 'Email does not appear to be valid').isEmail()
   ], async (req, res) => {
 
-  // check the validation object for errors
+    // check the validation object for errors
     let errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
+      return res.status(422).json({
+        errors: errors.array()
+      });
     }
-  let hashedPassword = Users.hashPassword(req.body.Password);  
-    await Users.findOne({ Username: req.body.Username }) // Search to see if a user with the requested username already exists
-        .then((user) => {
-          if (user) {
-            return res.status(400).send(req.body.Username + 'already exists');
-          } else {
-            Users
-              .create({
-                Username: req.body.Username,
-                Password: hashedPassword,
-                Email: req.body.Email,
-                Birthday: req.body.Birthday
-              })
-                .then((user) => {res.status(201).json(user) })
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    await Users.findOne({
+        Username: req.body.Username
+      }) // Search to see if a user with the requested username already exists
+      .then((user) => {
+        if (user) {
+          return res.status(400).send(req.body.Username + 'already exists');
+        } else {
+          Users
+            .create({
+              Username: req.body.Username,
+              Password: hashedPassword,
+              Email: req.body.Email,
+              Birthday: req.body.Birthday
+            })
+            .then((user) => {
+              res.status(201).json(user)
+            })
             .catch((error) => {
               console.error(error);
               res.status(500).send('Error: ' + error);
             });
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-          res.status(500).send('Error: ' + error);
-        });
-});
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
+  });
 
 // Update: a user's info, by username
 
-app.put("/users/:username", 
+app.put("/users/:username",
   //input validation here
-[
-  check('Username', 'Username is required').isLength({ min: 5}),
-  check('Username', 'Username contains non alphanumeric characters not allowed.').isAlphanumeric(),
-  check('Password', 'Password is required').not().isEmpty(),
-  check('Email', 'Email does not appear to be valid').isEmail()
-], passport.authenticate('jwt', {
-  session: false
-}), async (req, res) => {
+  [
+    check('Username', 'Username is required').isLength({
+      min: 5
+    }),
+    check('Username', 'Username contains non alphanumeric characters not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], passport.authenticate('jwt', {
+    session: false
+  }), async (req, res) => {
 
-  //check validation object for errors
-  let errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(422).json({
-      errors: errors.array()
-    });
-  }
+    //check validation object for errors
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        errors: errors.array()
+      });
+    }
 
-  //condition to check that username in request matches username in request params
-  if (req.user.username !== req.params.username) {
-    return res.status(400).send('Permission denied.');
-  }
-  //condition ends, finds user and updates their info
-  await Users.findOneAndUpdate({
-        Username: req.params.username
-      }, {
-        $set: {
-          Username: req.body.Username,
-          Password: req.body.Password,
-          Email: req.body.Email,
-          Birthday: req.body.Birthday,
-        },
-      }, {
-        new: true
-      } //this line makes sure the updated document is returned
-    )
-    .then((updatedUser) => {
-      res.json(updatedUser);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error:" + err);
-    });
-});
+    //condition to check that username in request matches username in request params
+    if (req.user.username !== req.params.username) {
+      return res.status(400).send('Permission denied.');
+    }
+    //condition ends, finds user and updates their info
+    await Users.findOneAndUpdate({
+          Username: req.params.username
+        }, {
+          $set: {
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday,
+          },
+        }, {
+          new: true
+        } //this line makes sure the updated document is returned
+      )
+      .then((updatedUser) => {
+        res.json(updatedUser);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error:" + err);
+      });
+  });
 // Add a movie to a user's list of favorites
 
 app.post("/users/:Username/movies/:MovieID", async (req, res) => {
@@ -305,6 +322,6 @@ app.get("/secreturl", (req, res) => {
 
 // Listen for requests
 const port = process.env.PORT || 8080;
-app.listen(port, '0.0.0.0',() => {
- console.log('Listening on Port ' + port);
+app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on Port ' + port);
 });
