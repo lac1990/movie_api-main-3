2523dee484b89bdcc8f996823bb85a1199d979c6
lac1990 <155014030+lac1990@users.noreply.github.com>
2024-05-23 13:44:09 -0500

    3.4
    remove passport from get movies

--- index.js
+++ index.js
@@ -242,9 +242,7 @@ app.get('/users/:Username', passport.authenticate('jwt', {
 });
 
 //get movies
-app.get('/movies', passport.authenticate('jwt', {
-  session: false
-}), async (req, res) => {
+app.get('/movies', async (req, res) => {
   await Movies.find()
     .then((movies) => {
       res.status(201).json(movies);
