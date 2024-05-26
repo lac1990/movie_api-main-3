69b3f3ee6b040eea1e43988ebeec299cd25de76c
lac1990 <155014030+lac1990@users.noreply.github.com>
2024-05-26 09:14:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
 
-let auth = require('./auth')(app);
+//let auth = require('./auth')(app);
 
 const passport = require('passport');
 require('./passport');
