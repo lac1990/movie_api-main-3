7cb1e180b30a56c4de3333e6d48da1a3eecbfcfb
lac1990 <155014030+lac1990@users.noreply.github.com>
2024-08-11 10:24:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -256,6 +256,16 @@ app.get("/movies/:Title", async (req, res) => {
       res.status(500).send("Error: " + err);
     });
 });
+app.get('/movies/:MovieID', passport.authenticate('jwt', { session: false }),  async (req, res) => { // Gets movie by ID
+  await Movies.findOne({ _id: req.params.MovieID })
+  .then((movies) => {
+      res.status(201).json(movies);
+  })
+  .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+  });
+});
 
 // GET genres from movies
 app.get("/movies/genre/:genreName", async (req, res) => {
