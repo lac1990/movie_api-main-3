ad7a52457875b5de7ab33ab3393e3c5e90ba3dcc
lac1990 <155014030+lac1990@users.noreply.github.com>
2024-08-13 09:20:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -27,8 +27,17 @@ app.use(express.urlencoded({
 app.use(bodyParser.json());
 
 const cors = require('cors');
-app.use(cors());
-
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234', 'https://my-awesome-site123.netlify.app'];
 let auth = require('./auth')(app);
 
 const passport = require('passport');
